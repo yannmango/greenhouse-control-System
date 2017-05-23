a7abf1343c38e4be1adb03f038b419338da4f74c
yannmango <yananchen047@gmail.com>
2017-05-23 14:56:58 -0400

    Add files via upload

---
+++ build-IOTer-Desktop_Qt_5_3_0_MinGW_32bit-Debug/debug/moc_MainWidget.cpp
@@ -0,0 +1,222 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'MainWidget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.3.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../ten_score/MainWidget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'MainWidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.3.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWidget_t {
+    QByteArrayData data[28];
+    char stringdata[514];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWidget_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWidget_t qt_meta_stringdata_MainWidget = {
+    {
+QT_MOC_LITERAL(0, 0, 10),
+QT_MOC_LITERAL(1, 11, 9),
+QT_MOC_LITERAL(2, 21, 0),
+QT_MOC_LITERAL(3, 22, 17),
+QT_MOC_LITERAL(4, 40, 19),
+QT_MOC_LITERAL(5, 60, 19),
+QT_MOC_LITERAL(6, 80, 13),
+QT_MOC_LITERAL(7, 94, 12),
+QT_MOC_LITERAL(8, 107, 22),
+QT_MOC_LITERAL(9, 130, 19),
+QT_MOC_LITERAL(10, 150, 15),
+QT_MOC_LITERAL(11, 166, 12),
+QT_MOC_LITERAL(12, 179, 24),
+QT_MOC_LITERAL(13, 204, 20),
+QT_MOC_LITERAL(14, 225, 15),
+QT_MOC_LITERAL(15, 241, 19),
+QT_MOC_LITERAL(16, 261, 13),
+QT_MOC_LITERAL(17, 275, 21),
+QT_MOC_LITERAL(18, 297, 21),
+QT_MOC_LITERAL(19, 319, 26),
+QT_MOC_LITERAL(20, 346, 26),
+QT_MOC_LITERAL(21, 373, 28),
+QT_MOC_LITERAL(22, 402, 28),
+QT_MOC_LITERAL(23, 431, 20),
+QT_MOC_LITERAL(24, 452, 20),
+QT_MOC_LITERAL(25, 473, 14),
+QT_MOC_LITERAL(26, 488, 12),
+QT_MOC_LITERAL(27, 501, 12)
+    },
+    "MainWidget\0close_all\0\0control_big_light\0"
+    "control_alarm_light\0control_night_light\0"
+    "close_curtain\0open_curtain\0"
+    "control_airconditioner\0open_airconditioner\0"
+    "control_add_wet\0open_add_wet\0"
+    "open_setting_port_widget\0show_about_us_widget\0"
+    "port_setting_Ok\0port_setting_Cancel\0"
+    "get_port_data\0show_temAndHum_widget\0"
+    "hide_temAndHum_widget\0show_temAndHum_line_widget\0"
+    "hide_temAndHum_line_widget\0"
+    "hide_temAndHum_and_show_line\0"
+    "hide_line_and_show_temAndHum\0"
+    "show_password_widget\0hide_password_widget\0"
+    "check_password\0shake_window\0switch_model"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWidget[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      26,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  144,    2, 0x08 /* Private */,
+       3,    0,  145,    2, 0x08 /* Private */,
+       4,    0,  146,    2, 0x08 /* Private */,
+       5,    0,  147,    2, 0x08 /* Private */,
+       6,    0,  148,    2, 0x08 /* Private */,
+       7,    0,  149,    2, 0x08 /* Private */,
+       8,    0,  150,    2, 0x08 /* Private */,
+       9,    0,  151,    2, 0x08 /* Private */,
+      10,    0,  152,    2, 0x08 /* Private */,
+      11,    0,  153,    2, 0x08 /* Private */,
+      12,    0,  154,    2, 0x08 /* Private */,
+      13,    0,  155,    2, 0x08 /* Private */,
+      14,    0,  156,    2, 0x08 /* Private */,
+      15,    0,  157,    2, 0x08 /* Private */,
+      16,    0,  158,    2, 0x08 /* Private */,
+      17,    0,  159,    2, 0x08 /* Private */,
+      18,    0,  160,    2, 0x08 /* Private */,
+      19,    0,  161,    2, 0x08 /* Private */,
+      20,    0,  162,    2, 0x08 /* Private */,
+      21,    0,  163,    2, 0x08 /* Private */,
+      22,    0,  164,    2, 0x08 /* Private */,
+      23,    0,  165,    2, 0x08 /* Private */,
+      24,    0,  166,    2, 0x08 /* Private */,
+      25,    0,  167,    2, 0x08 /* Private */,
+      26,    0,  168,    2, 0x08 /* Private */,
+      27,    0,  169,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWidget *_t = static_cast<MainWidget *>(_o);
+        switch (_id) {
+        case 0: _t->close_all(); break;
+        case 1: _t->control_big_light(); break;
+        case 2: _t->control_alarm_light(); break;
+        case 3: _t->control_night_light(); break;
+        case 4: _t->close_curtain(); break;
+        case 5: _t->open_curtain(); break;
+        case 6: _t->control_airconditioner(); break;
+        case 7: _t->open_airconditioner(); break;
+        case 8: _t->control_add_wet(); break;
+        case 9: _t->open_add_wet(); break;
+        case 10: _t->open_setting_port_widget(); break;
+        case 11: _t->show_about_us_widget(); break;
+        case 12: _t->port_setting_Ok(); break;
+        case 13: _t->port_setting_Cancel(); break;
+        case 14: _t->get_port_data(); break;
+        case 15: _t->show_temAndHum_widget(); break;
+        case 16: _t->hide_temAndHum_widget(); break;
+        case 17: _t->show_temAndHum_line_widget(); break;
+        case 18: _t->hide_temAndHum_line_widget(); break;
+        case 19: _t->hide_temAndHum_and_show_line(); break;
+        case 20: _t->hide_line_and_show_temAndHum(); break;
+        case 21: _t->show_password_widget(); break;
+        case 22: _t->hide_password_widget(); break;
+        case 23: _t->check_password(); break;
+        case 24: _t->shake_window(); break;
+        case 25: _t->switch_model(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject MainWidget::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_MainWidget.data,
+      qt_meta_data_MainWidget,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *MainWidget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWidget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWidget.stringdata))
+        return static_cast<void*>(const_cast< MainWidget*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int MainWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 26)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 26;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 26)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 26;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
